62104d0a4a41082b397f5156c04f871e3238feac
RAFAEL ADELUNGUE DA SILVA <rafael.asilva108@senacsp.edu.br>
2025-05-06 19:37:53 -0300

    criação do projeto

---
+++ CodigoPerdido/src/codigoperdido/CodigoPerdido.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package codigoperdido;
+
+/**
+ *
+ * @author rafael.asilva108
+ */
+public class CodigoPerdido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
