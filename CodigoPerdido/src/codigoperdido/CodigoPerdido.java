f94237efc207aeef4ade5dbcab88f17e036f8e38
unknown <gustavovianagon@gmail.com>
2025-05-26 13:31:48 -0300

    fases

--- CodigoPerdido/src/codigoperdido/CodigoPerdido.java
+++ CodigoPerdido/src/codigoperdido/CodigoPerdido.java
@@ -38,28 +38,33 @@ public class CodigoPerdido {
         input.nextLine();
     }
     
-    public static void inicio(){
+    public static String inicio(){
         Scanner input = new Scanner(System.in);
         String resposta;
         int numero;
+        String nome;
+       
+        
+        System.out.println("Digite o nome que você deseja: ");
+        nome = input.next();
 
         System.out.println("#--- Lore ---#\n");
         System.out.println("???: Você é mais uma pessoa comum nesse mundo que chamamos de lugar pra viver...\n\n" +
                 "???: Um dia você estava fazendo o que sempre fazia, quando nota um e-mail estranho...\n\n" +
-                "Jogador: Esse arquivo tá em um tipo diferente, .@#0. Estranho, será que é algum jogo crackeado? Vou abrir.\n\n" +
+                nome + ": Esse arquivo tá em um tipo diferente, .@#0. Estranho, será que é algum jogo crackeado? Vou abrir.\n\n" +
                 "???: Você abre o arquivo e vê...\n\n" +
-                "Jogador: Um monte de zeros e uns???? Que negócio nada a ver.\n\n" +
+                nome + ": Um monte de zeros e uns???? Que negócio nada a ver.\n\n" +
                 "???: Você decide digitar algo...\n\n" +
-                "Jogador: E se eu digitar 0???\n\n" +
+                nome + ": E se eu digitar 0???\n\n" +
                 "???: Um caderno ao seu lado desaparece.\n\n" +
-                "Jogador: Nem, NEM A PAU, EU SOU UM MANIPULADOR DE REALIDADE?\n\n" +
+                nome + ": Nem, NEM A PAU, EU SOU UM MANIPULADOR DE REALIDADE?\n\n" +
                 "???: Você brinca com a realidade até que...\n\n" +
-                "Jogador: E se eu apertar o dois??\n\n" +
+                nome + ": E se eu apertar o dois??\n\n" +
                 "???: Eu desisto... MAS VOCÊ QUEBROU A REALIDADE!\n\n" +
                 "Professor Enigma: Agora você está neste mundo para consertar o que fez.\n\n" +
-                "Jogador: Que lugar é esse?\n\n" +
+                nome + ": Que lugar é esse?\n\n" +
                 "Professor Enigma: Esse mundo mantém a realidade funcionando. Vamos consertar as falhas nas bases numéricas.\n\n" +
-                "Jogador: Binário? Octal? Hexa-o-quê?\n\n" +
+                nome + ": Binário? Octal? Hexa-o-quê?\n\n" +
                 "Professor Enigma: Vou te ensinar tudo.\n\n");
 
         System.out.println("Professor Enigma: Vamos começar com a primeira pergunta: O que é o sistema binário?\n\n");
@@ -79,7 +84,7 @@ public class CodigoPerdido {
         System.out.println("Crie uma tabela dos números em base 2 (de 1 até 1024). Vamos testar!\n\n");
 
         for (int i = 0; i < 11; i++) {
-            System.out.print("Jogador, digite um número da tabela binária: ");
+            System.out.print(nome + ", digite um número da tabela binária: ");
             numero = input.nextInt();
 
             if (numero == 1 || numero == 2 || numero == 4 || numero == 8 ||
@@ -101,15 +106,34 @@ public class CodigoPerdido {
         System.out.println("Soma: 8 + 4 + 1 = 13 --> Resultado: 00000001101\n\n");
 
         System.out.println("Professor Enigma: Agora sim, estamos prontos para os verdadeiros desafios.");
+        
+        
+        return nome;
+    }
+    
+    public static void cofreoctal(String nome){
+        
+    }
+    
+    public static void chavehexadecimal(String nome){
+        
+    }
+    
+    public static void bossfinal(String nome){
+        
     }
     
     public static void main(String[] args) {
+        
         int opcao;
      do{
         opcao = menu();
         switch (opcao){
          case 1:
-            inicio();
+            String nome = inicio();
+             cofreoctal(nome);
+             chavehexadecimal(nome);
+             bossfinal(nome);
             break;
          case 2:
              instrucoes();
